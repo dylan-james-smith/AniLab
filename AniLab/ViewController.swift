0bb3122ea6747eb73443820a064ff89d855873d4
Dylan James SMith <dsmith95@mail.ccsf.edu>
2016-03-23 19:13:31 -0700

    rotate arrow - enable counterclockwise and check if flipped. tweak animation speeds

--- AniLab/ViewController.swift
+++ AniLab/ViewController.swift
@@ -14,14 +14,16 @@ class ViewController: UIViewController {
     @IBOutlet var trayView: UIView!
     @IBOutlet weak var arrow: UIImageView!
     
+    var flippedUp: Bool?
     var trayOriginalCenter: CGPoint!
     var trayCenterWhenOpen: CGPoint?
     var trayCenterWhenClosed: CGPoint?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-         trayCenterWhenOpen = CGPoint(x: trayView.center.x, y: trayView.center.y - 150)
-         trayCenterWhenClosed = CGPoint(x: trayView.center.x, y: trayView.center.y)
+        trayCenterWhenOpen = CGPoint(x: trayView.center.x, y: trayView.center.y - 150)
+        trayCenterWhenClosed = CGPoint(x: trayView.center.x, y: trayView.center.y)
+        flipArrow("flipUp")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -29,18 +31,29 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func flipArrow(direction: String = ""){
+        let pi: CGFloat!
+        if direction == "left" || direction == "flipUp" || direction == "counterClockwise"{
+            pi = CGFloat(-3.14159265358979)
+            flippedUp = true
+        }else{
+            pi = CGFloat(M_PI)
+            flippedUp = false
+        }
+        arrow.transform = CGAffineTransformRotate(arrow.transform, pi)
+        }
 
     @IBAction func onTrayTap(sender: UITapGestureRecognizer) {
         if self.trayView.center == trayCenterWhenOpen {
-            print(">>>Click it down")
-            UIView.animateWithDuration(1, animations: { () -> Void in
+            UIView.animateWithDuration(0.4, animations: { () -> Void in
                 self.trayView.center = self.trayCenterWhenClosed!
+                self.flipArrow("flipUp")
             })
-            
         }else if self.trayView.center == trayCenterWhenClosed {
-            print(">>>Click it up")
-            UIView.animateWithDuration(0.8, animations: { () -> Void in
+            UIView.animateWithDuration(0.4, animations: { () -> Void in
                 self.trayView.center = self.trayCenterWhenOpen!
+                self.flipArrow()
             })
         }
     }
@@ -52,26 +65,33 @@ class ViewController: UIViewController {
         
         if pan.state == UIGestureRecognizerState.Began {
             trayOriginalCenter = trayView.center
-            
+        
         } else if pan.state == UIGestureRecognizerState.Changed {
             trayView.center = CGPoint(x: trayOriginalCenter.x, y: trayOriginalCenter.y + translation.y)
             
         } else if pan.state == UIGestureRecognizerState.Ended {
             
             if velocity > 0 {
-                UIView.animateWithDuration(0.4, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: velocity/100, options: [], animations: { () -> Void in
+                UIView.animateWithDuration(0.8, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 8, options: [], animations: { () -> Void in
                     self.trayView.center = self.trayCenterWhenClosed!
+                    if self.flippedUp == false {
+                        self.flipArrow("flipUp")
+                    }
                     }, completion: { (Bool) -> Void in
-                        
                 })
             }else if velocity < 0 {
-                UIView.animateWithDuration(0.5, animations: { () -> Void in
+                UIView.animateWithDuration(0.8, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 8, options: [], animations: { () -> Void in
                     self.trayView.center = self.trayCenterWhenOpen!
+                    if self.flippedUp == true {
+                        self.flipArrow()
+                    }
+                    }, completion: { (Bool) -> Void in
                 })
+                
             }
         }
     }
     
-
+    
 }
 
